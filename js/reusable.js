71c969e9af207b87e5a4d7fa72d95fbbfad7cefc
dimitrirozomashvili <georgiandito@gmail.com>
2024-03-20 20:17:54 +0400

    Changed toggleDarkAndLight() to small

--- js/reusable.js
+++ js/reusable.js
@@ -11,24 +11,12 @@ const burgerDlMode = document.querySelector(".burger-dlmode");
 const burgerClanguage = document.querySelector(".burger-clanguage");
 const burgerClanguageImg = burgerClanguage.querySelectorAll("a img");
 const burgerDlAllI = burgerDlMode.querySelectorAll("i");
-function toggleDarkAndLight(dark, light, classs) {
-  let darkEl = dark;
-  let lightEl = light
-  /* toggle dark and light mode
 
-  */
-  if (darkEl.style.display === "" || darkEl.style.display === "block") {
-    darkEl.style.display = "none";
-    document.querySelector(`${classs}`).style.justifyContent = "flex-end";
-    lightEl.style.display = "block";
-    document.body.classList.add("toggle-dark-mode")
-  } else {
-    lightEl.style.display = "none";
-    darkEl.style.display = "block";
-    document.querySelector(`${classs}`).style.justifyContent = "flex-start";
-    document.body.classList.remove("toggle-dark-mode");
-  }
+function toggleDarkAndLight() {
+  /* toggle dark and light mode*/
+  document.body.classList.toggle("toggle-dark-mode");
 }
+
 function changeLanguage(className) {
   /* When added it make georgia.png go display none,
   and at the same time it makes united-kingdom.png
@@ -40,7 +28,7 @@ function changeLanguage(className) {
 
 images.forEach(image => {
   image.addEventListener("click", () => {
-    toggleDarkAndLight(images[0], images[1], '.toggle-dark-and-light-mode-section');
+    toggleDarkAndLight();
   });
 });
 
@@ -48,7 +36,6 @@ navigationLanguageimages.forEach(image => {
   image.addEventListener("click", () => {
     // .navigation-language-bar
     // .burger-clanguage
-    console.log(image.closest("div").className)
     changeLanguage(image.closest("div").className.split(" "), 1);
   });
 });
@@ -56,13 +43,19 @@ navigationLanguageimages.forEach(image => {
 
 burgerDlAllI.forEach(image => {
   image.addEventListener("click", () => {
-    toggleDarkAndLight(burgerDlAllI[0], burgerDlAllI[1], ".burger-dlmode");
+    toggleDarkAndLight();
   });
 });
 
 burgerClanguageImg.forEach(image => {
   image.addEventListener("click", () => {
-    console.log(image.closest("div").className)
     changeLanguage(image.closest("div").className.split(" "), 1);
   });
 });
+
+
+// window.addEventListener("resize", () => {
+//     if (document.documentElement.clientWidth > 720) {
+
+//     }
+// });
