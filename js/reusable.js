23a57d514b68661b5eba104fcbf6b03746497c5a
dimitrirozomashvili <georgiandito@gmail.com>
2024-03-21 16:43:27 +0400

    Removed hover method from burger-bar
    From now on if you won't click second time to burger-bar, burger-menu won't disappear, also I transferred burger-menu to left side and if resolution is more more than 720px burger-menu will disappear also.

--- js/reusable.js
+++ js/reusable.js
@@ -11,7 +11,7 @@ const burgerDlMode = document.querySelector(".burger-dlmode");
 const burgerClanguage = document.querySelector(".burger-clanguage");
 const burgerClanguageImg = burgerClanguage.querySelectorAll("a img");
 const burgerDlAllI = burgerDlMode.querySelectorAll("i");
-
+const burgerSection = document.querySelector(".burger-section");
 function toggleDarkAndLight() {
   /* toggle dark and light mode*/
   document.body.classList.toggle("toggle-dark-mode");
@@ -53,9 +53,30 @@ burgerClanguageImg.forEach(image => {
   });
 });
 
-
-// window.addEventListener("resize", () => {
-//     if (document.documentElement.clientWidth > 720) {
-
-//     }
-// });
+/* 
+Why old algorithm worked was cause burger menu and burger bar were on right side of document
+so when you hovered on burger bar it instantly showed burger menu but it covered burger bar 
+from the top and only way to turn off burger menu was to not hover on burger menu. 
+Then I used click event instead of hover pseudo class on burger bar, but there was one problem
+burger-menu was covering burger-bar so I took burger menu to left and now when you click on 
+burger bar it shows burger menu to the left side of document, if I click it secodn time burger-menu
+will dissapear. 
+*/
+let count = 0;
+document.querySelector(".burger-section .fa-bars").addEventListener("click", () => {
+  count++;
+  document.querySelector(".burger-menu-section").classList.add("show-burger-menu");
+  if (count > 1) {
+    document.querySelector(".burger-menu-section").classList.remove("show-burger-menu");
+    count = 0;
+  }
+});
+// if document width is more than 720 it removes classList of show-burger-menu from .burger-menu-section
+// that means when you have res less than 720px and didn't click second time to burger-bar, show-burger-
+//menu classList won't be removed and because of that you would see both burger-menu and nav-bar.
+window.addEventListener("resize", () => {
+  if (document.documentElement.clientWidth > 720) {
+    document.querySelector(".burger-menu-section").classList.remove("show-burger-menu");
+    count = 0;
+  }
+});
